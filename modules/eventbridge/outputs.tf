ee5da998cc821577756345a69369439b07cb3514
amaruxia42 <amaruxia24@gmail.com>
2026-01-23 06:35:50 -0500

    fixed typo guardduty outputs.tf

--- modules/eventbridge/outputs.tf
+++ modules/eventbridge/outputs.tf
@@ -1,11 +1,10 @@
 # /cnapp_threat_detection/modules/eventbridge/outputs.tf
 output "event_rule_arn" {
   description = "ARN of the CloudWatch EventBridge rule"
-  value       = aws_cloudwatch_event_rule.guarduty_findings.arn
+  value       = aws_cloudwatch_event_rule.guardduty_findings.arn
 }
 
 output "event_rule_name" {
   description = "Name of CloudWatch EventBridge Rule"
-  value       = aws_cloudwatch_event_rule.guarduty_findings.name
+  value       = aws_cloudwatch_event_rule.guardduty_findings.name
 }
-
